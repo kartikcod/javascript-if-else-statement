0a5e45cd76e4183760fbd9f76ce3990c2997d3b3
kartik mahato <118644047+kartikcod@users.noreply.github.com>
2023-11-29 13:53:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// var obj={
+//     name:"kartik",
+//     Subject:"history",
+//     class:"12th",
+//     Collage_Rollno:"0121",
+//     collage_Name:"kashi sahu collage seraikella"
+// }
+// console.log(obj);
+// obj.rate="8000";
+// console.log(obj);
+
+// age= 15;
+// grace=3;
+// if((age+grace)>=18){
+//     console.log("your can vote");
+// }
+// else if(age >15){
+//     console.log("you can not vote deffentaly")
+// }
+// else{
+//     console.log("you can note vote");
+// }
+
+mark = 120;
+if(mark>450)
+{
+console.log("you can going to puri");
+}
+else if(mark==450)
+{
+    console.log("you can going uttrsakhand");
+}
+else
+{
+    console.log("you can note going to other place you place home");
+}
